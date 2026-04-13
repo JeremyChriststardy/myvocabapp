cefd084eb516477c5d0baebbd46d9d2c1bac3ba6
JeremyChriststardy <jeremychriststardy@gmail.com>
2026-04-14 01:44:14 +0800

    feat: setup EAS automation and environment variables

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zzenxkleifruvencdghu.supabase.co';
-const supabaseAnonKey = 'sb_publishable_yYM5mkRPBG_uRNYiVwUyfQ_mlbePCWF';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase Environment Variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
